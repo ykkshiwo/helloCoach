e32e63d683147f53ecf6b03abeabf535dc0aadd5
chengshitao <452306233@qq.com>
2019-07-04 22:41:50 +0800

    可以放大

--- miniprogram/pages/screenshot/screenshot.js
+++ miniprogram/pages/screenshot/screenshot.js
@@ -7,13 +7,16 @@ Page({
   data: {
     imageUrl: "./test.jpg",
     width: 375,
-    height: "",
+    height: 700,
     olddistance: 0, //上一次两个手指的距离
     newdistance: "", //本次两手指之间的距离，两个一减咱们就知道了滑动了多少，以及放大还是缩小（正负嘛）  
     diffdistance: '', //这个是新的比例，新的比例一定是建立在旧的比例上面的，给人一种连续的假象  
     Scale: 1, //图片放大的比例，
-    baseHeight: '', //原始高  
-    baseWidth: '', //原始宽
+    oldscaleA: 1,
+    baseHeight: 375, //原始高  
+    baseWidth: 700, //原始宽
+    scrollX: 0,
+    scrollY: 0,
   },
 
   //手指在屏幕上移动
@@ -26,7 +29,8 @@ Page({
       var distance = Math.sqrt(xMove * xMove + yMove * yMove); //根据勾股定理算出两手指之间的距离  
       if (_this.data.olddistance == 0) {
         _this.data.olddistance = distance; //要是第一次就给他弄上值，什么都不操作
-        // console.log("第一次");
+        console.log("第一次");
+        console.log(distance);
       } else {
         _this.data.newdistance = distance; //第二次就可以计算它们的差值了  
         _this.data.diffdistance = _this.data.newdistance - _this.data.olddistance; //两次差值
@@ -37,6 +41,8 @@ Page({
         } else if (_this.data.Scale < 1) { //缩小不能小于当前
           return;
         }
+        console.log("现在的倍数： ", _this.data.Scale);
+        console.log("现在的高度： ", _this.data.baseHeight);
         //刷新.wxml ，每次相乘，都是乘以图片的显示宽高
         _this.setData({
           height: _this.data.baseHeight * _this.data.Scale,
@@ -50,14 +56,21 @@ Page({
   //手指离开屏幕
   endTou: function(e) {
     this.data.olddistance = 0; //重置
-    this.getRect();
+    // this.getRect();
+  },
+
+  getDisdanceOfScroll: function(e){
+    console.log("距离左侧距离： ", e.detail.scrollLeft)
+    console.log("距离顶部距离： ", e.detail.scrollTop)
   },
 
   getRect: function() {
     var _this = this;
-    wx.createSelectorQuery().select('.FilePath').boundingClientRect(function(rect) {
-      _this.data.x = Math.abs(rect.left); //x坐标
-      _this.data.y = Math.abs(rect.top); //y坐标
+    wx.createSelectorQuery().select('#image').boundingClientRect(function(rect) {
+      console.log("scroll-view左侧的距离： ", Math.abs(rect.left));
+      console.log("scroll-view上面的距离： ", Math.abs(rect.top));
+      _this.data.scrollX = Math.abs(rect.left); //x坐标
+      _this.data.scrollY = Math.abs(rect.top); //y坐标
     }).exec()
   },
 

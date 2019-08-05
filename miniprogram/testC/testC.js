ce61759538095fd9b11ab82d722f2f1299da2f2f
chengshitao <452306233@qq.com>
2019-08-05 23:02:41 +0800

    自定义组件可以动态取得自身的位置。

--- miniprogram/testC/testC.js
+++ miniprogram/testC/testC.js
@@ -4,7 +4,7 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    thisId: String,
   },
 
   /**
@@ -12,6 +12,23 @@ Component({
    */
   data: {
 
+  },
+
+  lifetimes: {
+    attached: function(e) {
+      // 在组件实例进入页面节点树时执行
+      console.log("组件进入节点实例。");
+      console.log(this.id);
+      const query = wx.createSelectorQuery()
+      query.select("#" + this.id).boundingClientRect()
+      query.selectViewport().scrollOffset()
+      query.exec(function (res) {
+        console.log(res[0].top, res[0].left);
+      })
+    },
+    detached: function() {
+      // 在组件实例被从页面节点树移除时执行
+    },
   },
 
   /**
